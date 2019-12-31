6adba5877b094f435ff2b02d2b6deaf906e2ca40
Liyu Wang <liyuwang1985@gmail.com>
2019-12-31 18:35:37 +1100

    Added initial project files

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Liyu Wang on 31/12/19.
+//  Copyright © 2019 Liyu Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
